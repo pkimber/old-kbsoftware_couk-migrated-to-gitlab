e983f59692cae994b344b1df7f905986c29522fc
Patrick Kimber <code@pkimber.net>
2015-08-24 09:06:39 +0100

    move 'crm' from 'pkimber_net' #1052

--- README.rst
+++ README.rst
@@ -4,15 +4,19 @@ kbsoftware.co.uk
 WIP
 ===
 
+.. tip:: For information on moving the CRM data from ``pkimber_net`` to
+         ``kbsoftware_couk``, see ``project/migrations/0001_initial.py``.
+
 Move CRM from ``pkimber_net`` to ``kbsoftware_couk``::
 
   pg_dump test_pkimber_net_patrick -f test_pkimber_net_patrick.sql
-  # psql -U postgres test_pkimber_net_patrick
-  drop database test_kbsoftware_couk_patrick;
+  psql -X -U postgres -c "drop database test_kbsoftware_couk_patrick;"
   psql -X -U postgres -c "CREATE DATABASE test_kbsoftware_couk_patrick TEMPLATE=template0 ENCODING='utf-8';"
 
-  psql -d test_kbsoftware_couk_patrick -f test_pkimber_net_patrick.sql 2> out.log
+  psql -X -U postgres -d test_kbsoftware_couk_patrick -f test_pkimber_net_patrick.sql 2> out.log
   cat out.log
+
+  django-admin.py migrate --noinput
 
 .. important:: Don't forget to copy files (private and public).
 
---
+++ project/migrations/0001_initial.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def remove_references_to_old_tables(apps, schema_editor):
+    model = apps.get_model('contenttypes', 'ContentType')
+    model.objects.filter(app_label='block').delete()
+    model.objects.filter(app_label='cms').delete()
+    model.objects.filter(app_label='compose').delete()
+    model.objects.filter(app_label='original').delete()
+    model.objects.filter(app_label='project').delete()
+    model.objects.filter(app_label='web').delete()
+    pass
+
+
+class Migration(migrations.Migration):
+    """Migrate ``crm`` from ``pkimber_net`` to ``kbsoftware_couk``.
+
+    ``kbsoftware_couk`` does not use ``block`` or ``cms`` so try and remove all
+    traces of it.  This will *hopefully* allow a smooth migration to ``block``
+    and ``compose`` in future.
+
+    .. warning:: Do **NOT** change the order of the ``drop table`` statements.
+
+    """
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunSQL('drop table project_stripecontent'),
+        migrations.RunSQL('drop table cms_container'),
+        migrations.RunSQL('drop table cms_section'),
+        migrations.RunSQL('drop table block_viewurl'),
+        migrations.RunSQL('drop table cms_headerfooter'),
+        migrations.RunSQL('drop table cms_layout'),
+        migrations.RunSQL('drop table cms_moderatestate'),
+        migrations.RunSQL('drop table cms_page'),
+        migrations.RunSQL('drop table cms_templatesection'),
+        migrations.RunSQL('drop table cms_template'),
+        migrations.RunSQL('drop table compose_article'),
+        migrations.RunSQL('drop table compose_articleblock'),
+        migrations.RunSQL('drop table original_cms_simple'),
+        migrations.RunSQL('drop table original_cms_section'),
+        migrations.RunSQL('drop table web_main'),
+        migrations.RunSQL('drop table web_mainblock'),
+        migrations.RunSQL('drop table block_pagesection'),
+        migrations.RunSQL('drop table block_section'),
+        migrations.RunSQL('drop table block_page'),
+        migrations.RunSQL('drop table block_paginatedsection'),
+        migrations.RunSQL('drop table block_editstate'),
+        migrations.RunSQL('drop table block_moderatestate'),
+        # reversion
+        migrations.RunSQL("""
+            delete from reversion_version
+                where content_type_id in (
+                    select id from django_content_type where app_label in (
+                        'block', 'cms', 'compose', 'project', 'web'
+                    )
+                )
+        """),
+        migrations.RunPython(remove_references_to_old_tables),
+    ]
