79373f0cd96680a502693e8fe41ea3ff237b126e
Patrick Kimber <code@pkimber.net>
2015-11-08 18:25:46 +0000

    dash - tickets, notes and time records #980

--- settings/base.py
+++ settings/base.py
@@ -145,6 +145,7 @@ TEMPLATES = [
 DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
