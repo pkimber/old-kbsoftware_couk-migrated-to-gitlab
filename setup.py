42fd46b2a1d085ed462891b3b21a946f4fdc60c3
Patrick Kimber <code@pkimber.net>
2016-12-19 20:03:57 +0000

    version 0.0.30

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
             'templates/dash/*.*',
         ],
     },
-    version='0.0.29',
+    version='0.0.30',
     description='KB Software Ltd',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
