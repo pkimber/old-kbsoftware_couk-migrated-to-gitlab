91d25cd52c66cac22f1a540c6a3d7ce523b2a58f
Patrick Kimber <code@pkimber.net>
2017-03-28 13:16:39 +0100

    WIP: Access the Google Search Console API to report on analytics
    https://www.kbsoftware.co.uk/crm/ticket/2214/

---
+++ project/management/commands/seo.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+# -*- encoding: utf-8 -*-
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = "SEO #2214"
+
+    def handle(self, *args, **options):
+        """
+        613815634402-7kdsk6othf06q7p2cnpvk2v1eo5gpjf9.apps.googleusercontent.com
+        0rREUkNThBWmAyF6kiaRSV_S
+
+        """
+        self.stdout.write("{}".format(self.help))
+        self.stdout.write("Complete...")
